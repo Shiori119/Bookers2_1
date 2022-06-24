5350f99e8673412f1897fcae2b7d5e2a1cc0792b
EC2 Default User <ec2-user@ip-172-31-89-164.ec2.internal>
2022-06-24 15:19:45 +0000

    [Finish] 4-25

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+
+  before_action :correct_user,only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      render user_path(current_user.id)
+    end
+
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @users = User.all
+    @user = current_user
+  end
+
+
+    private
+
+  def correct_user
+      @user = User.find(params[:id])
+      @books = @user.books
+      redirect_to user_path(current_user) unless @user == current_user
+  end
+
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :books, only:[:new, :index, :show, :edit, :create]
-  root to: "homes#top"
-  get "/homes/about" => "homes#about",as:"about"
   devise_for :users
+  root to: "homes#top"
+  get "home/about" => "homes#about", as: "about"
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :index, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
